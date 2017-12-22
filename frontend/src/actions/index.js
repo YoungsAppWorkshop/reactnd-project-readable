489ee3e8f8eb52018ddd7e46ec9e49a1dac5a02e
Young Jung <ryviusm6@gmail.com>
2017-12-22 19:34:21 +0900

    refactor: delete unnecessary export statements

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -3,8 +3,8 @@ import * as API from '../utils/api'
 import * as Schema from '../schema'
 import { normalize } from 'normalizr'
 
-export const requestGetCategories = () => ({ type: types.REQUEST_GET_CATEGORIES })
-export const receiveGetCategories = categories => ({ type: types.RECEIVE_GET_CATEGORIES, categories })
+const requestGetCategories = () => ({ type: types.REQUEST_GET_CATEGORIES })
+const receiveGetCategories = categories => ({ type: types.RECEIVE_GET_CATEGORIES, categories })
 export const getCategories = () => dispatch => {
   dispatch(requestGetCategories())
   return API.getCategories().then(categories =>
@@ -14,8 +14,8 @@ export const getCategories = () => dispatch => {
 
 export const selectCategory = category => ({ type: types.SELECT_CATEGORY, category })
 
-export const requestGetPosts = () => ({ type: types.REQUEST_GET_POSTS })
-export const receiveGetPosts = posts => ({ type: types.RECEIVE_GET_POSTS, posts })
+const requestGetPosts = () => ({ type: types.REQUEST_GET_POSTS })
+const receiveGetPosts = posts => ({ type: types.RECEIVE_GET_POSTS, posts })
 export const getPosts = category => dispatch => {
   dispatch(requestGetPosts())
   return API.getPosts(category).then(posts => {
@@ -24,8 +24,8 @@ export const getPosts = category => dispatch => {
   })
 }
 
-export const requestAddPost = () => ({ type: types.REQUEST_ADD_POST })
-export const receiveAddPost = post => ({ type: types.RECEIVE_ADD_POST, post })
+const requestAddPost = () => ({ type: types.REQUEST_ADD_POST })
+const receiveAddPost = post => ({ type: types.RECEIVE_ADD_POST, post })
 export const addPost = post => dispatch => {
   dispatch(requestAddPost())
   return API.addPost(post).then(post => {
@@ -33,10 +33,10 @@ export const addPost = post => dispatch => {
   })
 }
 
-export const selectPost = postId => ({ type: types.SELECT_POST, postId })
-export const requestGetPost = () => ({ type: types.REQUEST_GET_POST })
-export const receiveGetPost = post => ({ type: types.RECEIVE_GET_POST, post })
-export const fetchPost = postId => dispatch => {
+const selectPost = postId => ({ type: types.SELECT_POST, postId })
+const requestGetPost = () => ({ type: types.REQUEST_GET_POST })
+const receiveGetPost = post => ({ type: types.RECEIVE_GET_POST, post })
+const fetchPost = postId => dispatch => {
   dispatch(requestGetPost())
   return API.getPost(postId).then(post => {
     dispatch(receiveGetPost(post))
@@ -51,8 +51,8 @@ export const fetchPostIfNeeded = postId => (dispatch, getState) => {
   return dispatch(selectPost(postId))
 }
 
-export const requestUpdatePost = () => ({ type: types.REQUEST_UPDATE_POST })
-export const receiveUpdatePost = post => ({ type: types.RECEIVE_UPDATE_POST, post })
+const requestUpdatePost = () => ({ type: types.REQUEST_UPDATE_POST })
+const receiveUpdatePost = post => ({ type: types.RECEIVE_UPDATE_POST, post })
 export const updatePost = post => dispatch => {
   dispatch(requestUpdatePost())
   return API.updatePost(post).then(post => {
@@ -60,8 +60,8 @@ export const updatePost = post => dispatch => {
   })
 }
 
-export const requestGetComments = id => ({ type: types.REQUEST_GET_COMMENTS, id })
-export const receiveGetComments = comments => ({ type: types.RECEIVE_GET_COMMENTS, comments })
+const requestGetComments = id => ({ type: types.REQUEST_GET_COMMENTS, id })
+const receiveGetComments = comments => ({ type: types.RECEIVE_GET_COMMENTS, comments })
 export const getComments = id => dispatch => {
   dispatch(requestGetComments())
   return API.getComments(id).then(comments => {
