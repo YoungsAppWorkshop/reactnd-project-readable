b258095e15a293e002d671388f843a2065dca812
Young Jung <ryviusm6@gmail.com>
2017-12-30 14:53:08 +0900

    feat: add sticky footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+const Footer = () => (
+  <footer className="footer">
+    <Container>
+      <span>© Young Jung 2017 - Udacity ReactND</span>
+    </Container>
+  </footer>
+)
+
+export default Footer
--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import Footer from '../components/Footer'
 import Header from '../containers/Header'
 import ListContainer from '../containers/ListContainer'
 import PostContainer from '../containers/PostContainer'
+import '../styles/App.css'
 
 const App = () => (
   <div id="app" className="app">
@@ -13,6 +15,7 @@ const App = () => (
       <Route path="/:category" component={ListContainer}/>
       <Route exact path="/" component={ListContainer}/>
     </Switch>
+    <Footer />
   </div>
 )
 
